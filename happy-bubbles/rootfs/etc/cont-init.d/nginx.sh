dbc36f83796a844f1b878037180ec1a671029810
Michael Rüedi <morti@1337hium.ch>
2023-12-20 21:00:24 +0100

    fix2

--- happy-bubbles/rootfs/etc/cont-init.d/nginx.sh
+++ happy-bubbles/rootfs/etc/cont-init.d/nginx.sh
@@ -31,10 +31,10 @@ if bashio::var.has_value "${port}"; then
     sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
 fi
 
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-
-dns_host=$(bashio::dns.host)
-sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
+#ingress_port=$(bashio::addon.ingress_port)
+#ingress_interface=$(bashio::addon.ip_address)
+#sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+#sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+#
+#dns_host=$(bashio::dns.host)
+#sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
